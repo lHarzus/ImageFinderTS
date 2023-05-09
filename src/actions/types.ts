10a7412deb53d7460f2fb070e6e816d6a724f647
Ruben <rubnitos@hotmail.com>
2023-05-09 18:39:31 +0100

    redux working with api

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,7 +1,22 @@
 export const GET_IMAGES = "GET_IMAGES";
 export const IMAGES_ERROR = "IMAGES_ERROR";
 
-export type SuggestedSearch = {
+export type Images = {
+  url: string;
+  thumbnail: string;
+  title: string;
+  webpageUrl: string;
+};
+
+export interface GetImages {
+  type: typeof GET_IMAGES;
+  payload: {
+    value: Images[];
+    totalCount: number;
+  };
+}
+
+/*export type SuggestedSearch = {
   suggested_searches: {
     name: string;
     serpapi_link: string;
@@ -24,7 +39,7 @@ export interface GetImages {
     suggested_searches: SuggestedSearch[];
     images_results: ImagesSearched[];
   };
-}
+}*/
 
 export interface ImagesError {
   type: typeof IMAGES_ERROR;
